95c76a331345ae3b56f0c441445478a10107741a
mahayasa1 <mahayasa.udit@gmail.com>
2024-01-22 16:55:43 +0800

    g

--- koneksi.php
+++ koneksi.php
@@ -2,6 +2,23 @@
 
 $koneksi = mysqli_connect("localhost", "root", "", "db_jerriz");
 
+function cekSession() {
+    if (!isset($_SESSION['id_user'])) {
+        header("location:index.php?pesan=login");
+    }
+}
+
+function cekCookies() {
+    if (!isset($_SESSION['id_user'])) {
+        header("location:index.php?pesan=login");
+    }
+}
+
+function levelAdmin() {
+    if ($_SESSION['level'] != 0) {
+        header("location:beranda.php");
+    }
+}
 
 
 ?>
